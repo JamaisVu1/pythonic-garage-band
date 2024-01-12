4bd99737a7f077785276bb8e048fb2971cc37276
zach <zach.moore0628@gmail.com>
2024-01-11 18:28:41 -0600

    tests passing

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,10 +1,20 @@
 class Band:
     count = 0
+    instances = []
     
     def __init__(self, name, members):
         self.name = name
         Band.count += 1
         self.members = members
+        Band.instances.append(self)
+        
+    def play_solos(self):
+        return [member.play_solo() for member in self.members]
+    
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+        
 
 
 
@@ -17,7 +27,7 @@ class Musician:
 
 
 
-class Guitarist:
+class Guitarist(Musician):
     count = 0
 
     def __init__(self, name="unknown"):
@@ -33,8 +43,10 @@ class Guitarist:
     def get_instrument(self):
         return "guitar"
 
+    def play_solo(self):
+        return "face melting guitar solo"
 
-class Bassist:
+class Bassist(Musician):
     count = 0
 
     def __init__(self, name="unknown"):
@@ -50,7 +62,10 @@ class Bassist:
     def get_instrument(self):
         return "bass"
 
-class Drummer:
+    def play_solo(self):
+        return "bom bom buh bom"
+
+class Drummer(Musician):
     count = 0
 
     def __init__(self, name="unknown"):
@@ -65,3 +80,6 @@ class Drummer:
     
     def get_instrument(self):
         return "drums"
+    
+    def play_solo(self):
+        return "rattle boom crash"
